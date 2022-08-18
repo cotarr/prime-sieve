c0f9d6e8a8333e7da407a97b1adae7761719fafe
Dave Bolenbaugh <cotarr@yahoo.com>
2022-08-17 19:57:14 -0500

    Page tab settings

--- primes.c
+++ primes.c
@@ -6,45 +6,43 @@
 #include <stdio.h>
 
 #define PRIMELIMIT 100
-// Array dimension must be PRIMELIMIT + 1
-#define ARRAYSIZE 101
 
-unsigned char number_set[ARRAYSIZE];
+unsigned char number_set[PRIMELIMIT + 1];
 
 int  main()
 {
 
-  int i, j;      // loop variables
-  int count = 0; // Count of number of primes found
+    int i, j;      // loop variables
+    int count = 0; // Count of number of primes found
 
-  // Initialize array
-  for (i = 1; i < PRIMELIMIT + 1; i++)
-  {
-    // Value of 0 indicates not prime
-    // All numbers are possible primes until sieved.
-    number_set[i] = 1;
-  }
-
-  printf("\r\nSearching for prime numbers from 2 to %d.\r\n\r\n", PRIMELIMIT);
-
-  for (i = 2; i < PRIMELIMIT + 1; i++)
-  {
-    // Test i to see if value of i is prime (not prime when i == 0)
-    if (number_set[i] == 1)
+    // Initialize array
+    for (i = 1; i < PRIMELIMIT + 1; i++)
     {
-      // Value of i is not a multiple of a previous prime, so i is prime
-      printf("%d ", i);
-      count++;
-      // loop incrementing j by steps of current value of i
-      for (j = i; j < PRIMELIMIT + 1; j += i)
-      {
-        // Mark all multiples of i as NOT prime
-        number_set[j] = 0;
-      }
-    } 
-  }
+        // Value of 0 indicates not prime
+        // All numbers are possible primes until sieved.
+        number_set[i] = 1;
+    }
 
-  printf("\r\n\r\nFound %d prime numbers.\r\n\r\n", count);
+    printf("\r\nSearching for prime numbers from 2 to %d.\r\n\r\n", PRIMELIMIT);
 
-  return 0;
+    for (i = 2; i < PRIMELIMIT + 1; i++)
+    {
+        // Test i to see if value of i is prime (not prime when i == 0)
+        if (number_set[i] == 1)
+        {
+            // Value of i is not a multiple of a previous prime, so i is prime
+            printf("%d ", i);
+            count++;
+            // loop incrementing j by steps of current value of i
+            for (j = i; j < PRIMELIMIT + 1; j += i)
+            {
+                // Mark all multiples of i as NOT prime
+                number_set[j] = 0;
+            }
+        } 
+    }
+
+    printf("\r\n\r\nFound %d prime numbers.\r\n\r\n", count);
+
+    return 0;
 }
